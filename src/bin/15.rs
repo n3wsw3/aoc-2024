1a876d93771a7208d24a2c6525e86fa8ff6225e5
Simon Uggla <simon.uggla@gmail.com>
2024-12-18 00:16:16 +0100

    Restructure 15

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -70,7 +70,7 @@ impl FromStr for Direction {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Square2 {
+enum Square {
     Wall,
     BoxL,
     BoxR,
@@ -79,22 +79,22 @@ enum Square2 {
     Empty,
 }
 
-struct Map2 {
-    squares: Vec<Vec<Square2>>,
+struct Map {
+    squares: Vec<Vec<Square>>,
     robot: Position,
 }
 
-impl Display for Map2 {
+impl Display for Map {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         for row in &self.squares {
             for square in row {
                 let c = match square {
-                    Square2::Wall => '#',
-                    Square2::BoxL => '[',
-                    Square2::BoxR => ']',
-                    Square2::Robot => '@',
-                    Square2::Empty => '.',
-                    Square2::Box => 'O',
+                    Square::Wall => '#',
+                    Square::BoxL => '[',
+                    Square::BoxR => ']',
+                    Square::Robot => '@',
+                    Square::Empty => '.',
+                    Square::Box => 'O',
                 };
                 write!(f, "{}", c)?;
             }
@@ -104,53 +104,41 @@ impl Display for Map2 {
     }
 }
 
-impl Map2 {
-    fn parse_1(s: &str) -> Result<Self, String> {
+#[derive(Debug, Clone, Copy)]
+enum Version {
+    PartOne,
+    PartTwo,
+}
+
+impl Map {
+    fn parse(s: &str, version: Version) -> Result<Self, String> {
         let mut squares = Vec::new();
         let mut robot = Position { x: 0, y: 0 };
 
         for (y, line) in s.lines().enumerate() {
             let mut row = Vec::new();
             for (x, c) in line.chars().enumerate() {
-                let mut squares = match c {
-                    '#' => vec![Square2::Wall],
-                    'O' => vec![Square2::Box],
-                    '@' => {
+                let mut squares = match (version, c) {
+                    (Version::PartOne, '#') => vec![Square::Wall],
+                    (Version::PartTwo, '#') => vec![Square::Wall, Square::Wall],
+                    (Version::PartOne, 'O') => vec![Square::Box],
+                    (Version::PartTwo, 'O') => vec![Square::BoxL, Square::BoxR],
+                    (Version::PartOne, '@') => {
                         robot = Position {
                             x: x as i32,
                             y: y as i32,
                         };
-                        vec![Square2::Robot]
+                        vec![Square::Robot]
                     }
-                    '.' => vec![Square2::Empty],
-                    _ => return Err(format!("Unknown character: {}", c)),
-                };
-                row.append(&mut squares);
-            }
-            squares.push(row);
-        }
-
-        Ok(Map2 { squares, robot })
-    }
-
-    fn parse_2(s: &str) -> Result<Self, String> {
-        let mut squares = Vec::new();
-        let mut robot = Position { x: 0, y: 0 };
-
-        for (y, line) in s.lines().enumerate() {
-            let mut row = Vec::new();
-            for (x, c) in line.chars().enumerate() {
-                let mut squares = match c {
-                    '#' => vec![Square2::Wall, Square2::Wall],
-                    'O' => vec![Square2::BoxL, Square2::BoxR],
-                    '@' => {
+                    (Version::PartTwo, '@') => {
                         robot = Position {
                             x: (x as i32) * 2,
                             y: y as i32,
                         };
-                        vec![Square2::Robot, Square2::Empty]
+                        vec![Square::Robot, Square::Empty]
                     }
-                    '.' => vec![Square2::Empty, Square2::Empty],
+                    (Version::PartOne, '.') => vec![Square::Empty],
+                    (Version::PartTwo, '.') => vec![Square::Empty, Square::Empty],
                     _ => return Err(format!("Unknown character: {}", c)),
                 };
                 row.append(&mut squares);
@@ -158,7 +146,7 @@ impl Map2 {
             squares.push(row);
         }
 
-        Ok(Map2 { squares, robot })
+        Ok(Map { squares, robot })
     }
     fn can_move(&self, direction: Direction, position: Position) -> bool {
         let move_vector = direction.to_position();
@@ -167,19 +155,19 @@ impl Map2 {
         let current_square = self.squares[position.y as usize][position.x as usize];
 
         match (direction, current_square) {
-            (Direction::Up | Direction::Down, Square2::BoxL) => {
+            (Direction::Up | Direction::Down, Square::BoxL) => {
                 self.can_move(direction, new_position)
                     && self.can_move(direction, new_position + Direction::Right.to_position())
             }
-            (Direction::Up | Direction::Down, Square2::BoxR) => {
+            (Direction::Up | Direction::Down, Square::BoxR) => {
                 self.can_move(direction, new_position)
                     && self.can_move(direction, new_position + Direction::Left.to_position())
             }
-            (_, Square2::BoxL | Square2::BoxR) => {
+            (_, Square::BoxL | Square::BoxR) => {
                 self.can_move(direction, new_position + move_vector)
             }
-            (_, Square2::Wall) => false,
-            (_, Square2::Empty) => true,
+            (_, Square::Wall) => false,
+            (_, Square::Empty) => true,
             _ => self.can_move(direction, new_position),
         }
     }
@@ -191,8 +179,8 @@ impl Map2 {
         let current_square = self.squares[position.y as usize][position.x as usize];
 
         match (direction, current_square) {
-            (Direction::Up | Direction::Down, Square2::BoxR | Square2::BoxL) => {
-                let side = if current_square == Square2::BoxL {
+            (Direction::Up | Direction::Down, Square::BoxR | Square::BoxL) => {
+                let side = if current_square == Square::BoxL {
                     Direction::Right
                 } else {
                     Direction::Left
@@ -200,33 +188,33 @@ impl Map2 {
                 self.try_move_objects(direction, new_position);
                 self.try_move_objects(direction, new_position + side.to_position());
 
-                let other_square = if current_square == Square2::BoxL {
-                    Square2::BoxR
+                let other_square = if current_square == Square::BoxL {
+                    Square::BoxR
                 } else {
-                    Square2::BoxL
+                    Square::BoxL
                 };
                 let other = position + side.to_position();
                 let new_other = other + move_vector;
 
                 self.squares[new_position.y as usize][new_position.x as usize] = current_square;
                 self.squares[new_other.y as usize][new_other.x as usize] = other_square;
-                self.squares[position.y as usize][position.x as usize] = Square2::Empty;
-                self.squares[other.y as usize][other.x as usize] = Square2::Empty;
+                self.squares[position.y as usize][position.x as usize] = Square::Empty;
+                self.squares[other.y as usize][other.x as usize] = Square::Empty;
             }
-            (_, Square2::BoxL | Square2::BoxR) => {
+            (_, Square::BoxL | Square::BoxR) => {
                 self.try_move_objects(direction, new_position);
 
                 self.squares[new_position.y as usize][new_position.x as usize] = current_square;
-                self.squares[position.y as usize][position.x as usize] = Square2::Empty;
+                self.squares[position.y as usize][position.x as usize] = Square::Empty;
             }
-            (_, Square2::Empty) => {}
+            (_, Square::Empty) => {}
             _ => {
                 self.try_move_objects(direction, new_position);
 
                 self.squares[new_position.y as usize][new_position.x as usize] = current_square;
-                self.squares[position.y as usize][position.x as usize] = Square2::Empty;
+                self.squares[position.y as usize][position.x as usize] = Square::Empty;
 
-                if current_square == Square2::Robot {
+                if current_square == Square::Robot {
                     self.robot = new_position;
                 }
             }
@@ -240,30 +228,16 @@ impl Map2 {
 
         self.try_move_objects(direction, self.robot);
     }
-}
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (first, last) = input.split_once("\n\n")?;
-    let mut map = Map2::parse_1(first).ok()?;
-    let instructions: Vec<Direction> = last
-        .chars()
-        .map(|c| c.to_string().parse())
-        .flatten()
-        .collect_vec();
-
-    for direction in instructions {
-        map.move_robot(direction);
-    }
-
-    Some(
-        map.squares
+    fn calculate_gps_coords_sum(&self) -> u32 {
+        self.squares
             .iter()
             .enumerate()
             .map(|(x, row)| {
                 row.iter()
                     .enumerate()
-                    .filter_map(move |(y, square)| {
-                        if let Square2::Box = square {
+                    .filter_map(move |(y, &square)| {
+                        if square == Square::Box || square == Square::BoxL {
                             Some(x * 100 + y)
                         } else {
                             None
@@ -272,47 +246,42 @@ pub fn part_one(input: &str) -> Option<u32> {
                     .collect_vec()
             })
             .flatten()
-            .sum::<usize>() as u32,
-    )
+            .sum::<usize>() as u32
+    }
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    let (first, last) = input.split_once("\n\n")?;
-    let mut map: Map2 = Map2::parse_2(first).ok()?;
-    let instructions: Vec<Direction> = last
-        .chars()
+fn parse_instructions(s: &str) -> Vec<Direction> {
+    s.chars()
         .map(|c| c.to_string().parse())
         .flatten()
-        .collect_vec();
+        .collect_vec()
+}
+
+fn parse_input(s: &str, version: Version) -> (Map, Vec<Direction>) {
+    let (first, last) = s.split_once("\n\n").unwrap();
+    let map = Map::parse(first, version).unwrap();
+    let instructions = parse_instructions(last);
+    (map, instructions)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (mut map, instructions) = parse_input(input, Version::PartOne);
 
     for direction in instructions {
         map.move_robot(direction);
     }
 
-    let left_boxes = map
-        .squares
-        .iter()
-        .enumerate()
-        .map(|(y, row)| {
-            row.iter()
-                .enumerate()
-                .filter_map(move |(x, square)| {
-                    if let Square2::BoxL = square {
-                        Some(Position {
-                            x: x as i32,
-                            y: y as i32,
-                        })
-                    } else {
-                        None
-                    }
-                })
-                .collect_vec()
-        })
-        .flatten()
-        .map(|pos| pos.y * 100 + pos.x)
-        .sum::<i32>();
+    Some(map.calculate_gps_coords_sum())
+}
 
-    Some(left_boxes as u32)
+pub fn part_two(input: &str) -> Option<u32> {
+    let (mut map, instructions) = parse_input(input, Version::PartTwo);
+
+    for direction in instructions {
+        map.move_robot(direction);
+    }
+
+    Some(map.calculate_gps_coords_sum())
 }
 
 #[cfg(test)]
